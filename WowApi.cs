456f29fd41c2eb331ffc124d07b2fbda5da44de5
pjk_software <pjk@pjk.me>
2022-09-13 22:59:25 +0300

    Tracking 404 errors

--- WowApi.cs
+++ WowApi.cs
@@ -88,6 +88,14 @@ namespace Feralas
                 LogMaker.LogToTable($"WowApi", $"Blizzard sent an empty string for {tag}");
             }
 
+            if (auctionsJson.Contains("404"))
+            {
+
+                LogMaker.LogToTable($"WowApi", $"404 for {tag}");
+
+                LogMaker.LogToTable($"WowApi", $"{url}");
+            }
+
             return auctionsJson;
         }
 
